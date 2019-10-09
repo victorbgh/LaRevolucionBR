8f961330f8b8940b974386c820737dd3cf748993
Victor Hugo Goncalves de Brito <victorhugo-brito@hotmail.com>
2019-10-09 15:08:47 -0300

    add slide

--- js/main.js
+++ js/main.js
@@ -282,3 +282,34 @@ function underAge(){
 // 	let formContact = $('#clients')
 // 	formContact.find('input[name="telefone"]').mask('(00) 0000-0000');
 // });
+
+
+var options = {
+	prevNextButtons: false,
+	autoPlay: 3000,
+	freeScroll: true,
+	wrapAround: true,
+	accessibility: true,
+	pageDots: true,
+	setGallerySize: false,
+	// arrowShape: {
+	//   x0: 10,
+	//   x1: 60,
+	//   y1: 50,
+	//   x2: 60,
+	//   y2: 45,
+	//   x3: 15
+	// }
+  };
+  
+  var carousel = document.querySelector('[data-carousel]');
+  var slides = document.getElementsByClassName('carousel-cell');
+  var flkty = new Flickity(carousel, options);
+  
+  flkty.on('scroll', function () {
+	flkty.slides.forEach(function (slide, i) {
+	  var image = slides[i];
+	  var x = (slide.target + flkty.x) * -1/3;
+	//   image.style.backgroundPosition = x + 'px';
+	});
+  });
